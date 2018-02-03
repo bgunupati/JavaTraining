d22c43e47e0844c77b96ba954952a335fff5bc7f
Bharath Gunupati <bgunupati@gmail.com>
2018-02-02 20:58:57 -0500

    Java_Module-1

--- JavaSample/src/HelloJava.java
+++ JavaSample/src/HelloJava.java
@@ -2,8 +2,6 @@
 public class HelloJava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		System.out.println("Hello Java ! Welcome to the world of Open Source !!!");
+	} 
 }
